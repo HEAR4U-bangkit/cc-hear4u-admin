4759b8e8f3de28b1f535a8560ac31b7557d33158
dafariski77 <rskdafa@gmail.com>
2024-06-11 18:56:37 +0700

    feat: add hooks for user management

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -1,5 +1,5 @@
 import axiosInstance from "@/libs/axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useGetProfile = (token) => {
   return useQuery({
@@ -11,5 +11,60 @@ export const useGetProfile = (token) => {
         },
       });
     },
+  });
+};
+
+export const useGetOneUser = (userId, token) => {
+  return useQuery({
+    queryKey: [`user-${userId}`],
+    queryFn: async () => {
+      return await axiosInstance.get(`/user/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+  });
+};
+
+export const useCreateUser = ({ onSuccess, onError }) => {
+  return useMutation({
+    mutationFn: async (token, body) => {
+      return await axiosInstance.post("/user", body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onError,
+    onSuccess,
+  });
+};
+
+export const useUpdateUser = ({ onSuccess, onError }) => {
+  return useMutation({
+    mutationFn: async (token, userId, body) => {
+      return await axiosInstance.put(`/user/${userId}`, body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess,
+    onError,
+  });
+};
+
+export const useDeleteUser = ({ onSuccess, onError }) => {
+  return useMutation({
+    mutationFn: async (token, userId) => {
+      return await axiosInstance.delete(`/user/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess,
+    onError,
   });
 };
