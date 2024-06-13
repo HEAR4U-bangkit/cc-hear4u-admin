fc79974ca504b003e929dac030f88da444579764
dafariski77 <rskdafa@gmail.com>
2024-06-13 17:36:06 +0700

    style(button): add button variant

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-export default function Button({ text, ...rest }) {
-  return (
-    <button
-      className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
-      {...rest}
-    >
-      {text}
-    </button>
-  );
+export default function Button({ variant = "solid", text, ...rest }) {
+  if (variant == "solid") {
+    return (
+      <button
+        className="inline-flex items-center justify-center rounded-md bg-primary px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+        {...rest}
+      >
+        {text}
+      </button>
+    );
+  } else if (variant == "outline") {
+    return (
+      <button
+        className="inline-flex items-center justify-center rounded-md border border-primary px-10 py-4 text-center font-medium text-primary hover:bg-opacity-90 lg:px-8 xl:px-10"
+        {...rest}
+      >
+        {text}
+      </button>
+    );
+  }
 }
